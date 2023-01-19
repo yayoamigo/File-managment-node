13d1ade6bf12a79b7a74dce3b30426508f220b8d
david zambrano corral <Yayoamigo>
2023-01-19 11:14:59 -0500

    Encode and decode file

--- app.js
+++ app.js
@@ -2,12 +2,27 @@ const fs = require('fs/promises');
 
 ( async () => {
     const fileHandler = await fs.open('./commands.txt', 'r');
+    fileHandler.on('change', async ()=>{
+        console.log('the file was changed');
+            const size = ( await fileHandler.stat()).size;
+            const buff = Buffer.alloc(size);
+            const offset = 0;
+            const length  = buff.byteLength;
+            const position = 0;
+            await fileHandler.read(buff,offset,length,position);
+           const command = buff.toString();
+
+           if (command.includes('create a file')){
+            const filePath = command.substring('create a file' + 1)
+           }
+    })
 
     const watcher = fs.watch('./commands.txt');
-
     for await(const event of watcher){
         if(event.eventType === 'change' ){
-            console.log('the file was changed')
+
+            fileHandler.emit('change')
+            
         }
     }
 }) ();
